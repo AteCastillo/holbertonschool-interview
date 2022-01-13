9924899dfc81a24c08d265002db14da618c4506a
AteCastillo <atecast@gmail.com>
2022-01-13 15:39:49 -0300

    fixing checks

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,54 +1,120 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include "sandpiles.h"
-void stabilize_sandpiles(int grid[3][3]);
+
 /**
-* sandpiles_sum - function
-* @grid1: sandpile to add
-* @grid2: sandpile to addd to grid1
-* Description: compues the sums of two sandpiles
-* Return: sum
-*/
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ */
+
+static void print_grid(int grid[3][3])
 {
 	int i, j;
-	int rows = 3, columns = 3;
-	/* bool stable = true;*/
 
-	for (i = 0; i < rows; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < columns; j++)
+		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] = grid1[i][j] + grid2[i][j];
-			/* if (grid1[i][j] > 3)*/
-				/* stable = false;*/
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
 		}
+		printf("\n");
 	}
-	stabilize_sandpiles(grid1);
 }
 
 /**
-* stabilize_sandpiles - function
-* @grid: grid to stabilize
-*
-* Description: function to stabilize grid
-* Return: nothing
-*/
-void stabilize_sandpiles(int grid[3][3])
+ * is_stable - checks the stability of a sandpile
+ * @grid: 3x3 grid to test
+ * Return: 1 if stable, 0 else
+ */
+
+int is_stable(int grid[3][3])
 {
-	int rows, columns;
+	int i, j = 0;
 
-	for (columns = 0; columns < 3; columns++)
+	for (i = 0; i < 3; i++)
 	{
-		for (rows = 0; rows < 3; rows++)
+		for (j = 0; j < 3; j++)
 		{
-			if (grid[columns][rows] > 3)
-			{
-				grid[columns][rows] -= 4;
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
+}
 
-				if (columns - 1 >= 0)
-					grid[columns - 1][rows] += 1;
+/**
+ * sandpile_toppling - topples the sandpile to make it stable
+ * @grid1: the 3x3 sandpile
+ * @grid2: an empty 3x3 grid
+ */
+
+void sandpile_toppling(int grid1[3][3], int grid2[3][3])
+{
+	int x, y = 0;
+
+	while (y < 3)
+	{
+		x = 0;
+		while (x < 3)
+		{
+			if (grid1[y][x] > 3)
+			{
+				grid2[y][x] += grid1[y][x] - 4;
+				if (y - 1 >= 0)
+					grid2[y - 1][x] += 1;
+				if (x - 1 >= 0)
+					grid2[y][x - 1] += 1;
+				if (y + 1 < 3)
+					grid2[y + 1][x] += 1;
+				if (x + 1 < 3)
+					grid2[y][x + 1] += 1;
+			}
+			else
+				grid2[y][x] += grid1[y][x];
+			x++;
+		}
+		y++;
+	}
+}
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int x, y = 0;
+	int i, j = 0;
+
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			grid1[x][y] = grid1[x][y] + grid2[x][y];
+		}
+	}
+	while (is_stable(grid1) != 1)
+	{
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid2[i][j] = 0;
+			}
+		}
+		printf("=\n");
+		print_grid(grid1);
+		sandpile_toppling(grid1, grid2);
+
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid1[i][j] = grid2[i][j];
 			}
 		}
 	}
