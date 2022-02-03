593ffe038b6d0b1b8c67a45a148e8f70352a26f1
AteCastillo <atecast@gmail.com>
2022-02-03 12:54:58 -0300

    fixing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,38 +1,48 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+"""
+Python script takes URL from stdin and compute exact metrics
+"""
 import sys
-
-
-def formatPrint(size, myDict):
-    """format de dictionary"""
-    print("File size: {}".format(size))
-    for key, value in sorted(statusDict.items()):
-        if (value != 0):
-            print("{}: {}".format(key, value))
+import traceback
 
 
 if __name__ == "__main__":
-    """init code to print the parsed data"""
-    statusList = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    statusDict = {}
-    size = 0
-    i = 0
+
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    file_list = []
+    stat_dict = {}
+    file_size = 0
+    line_num = 0
+
+    for stat in codes:
+        stat_dict[stat] = 0
     try:
         for line in sys.stdin:
-            i += 1
-            if (len(line.split()) < 2):
-                continue
-            lineList = line.split()
-            statusCode = lineList[-2]
-            size = size + int(lineList[-1])
-            if statusCode in statusList:
-                try:
-                    statusDict[statusCode] += 1
-                except KeyError:
-                    statusDict[statusCode] = 1
-            if i % 10 == 0:
-                formatPrint(size, statusDict)
-        formatPrint(size, statusDict)
-    except KeyboardInterrupt:
-        formatPrint(size, statusDict)
+            try:
+                file_list = line.split(" ")
+                flist_length = len(file_list)
+                if flist_length != 9:
+                    pass
+                if file_list[-2] in codes:
+                    stat_dict[file_list[-2]] += 1
+                if file_list[-1][-1] == '\n':
+                    file_list[-1][:-1]
+                file_size += int(file_list[-1])
+            except:
+                pass
+            line_num += 1
+            if line_num % 10 == 0:
+                print("File size: {}".format(file_size))
+                for stat in sorted(stat_dict.keys()):
+                    if stat_dict[stat] != 0:
+                        print("{}: {}".format(stat, stat_dict[stat]))
+        print("File size: {}".format(file_size))
+        for stat in sorted(stat_dict.keys()):
+            if stat_dict[stat] != 0:
+                print("{}: {}".format(stat, stat_dict[stat]))
+    except KeyboardInterrupt as error:
+        print("File size: {}".format(file_size))
+        for stat in sorted(stat_dict.keys()):
+            if stat_dict[stat] != 0:
+                print("{}: {}".format(stat, stat_dict[stat]))
         raise
